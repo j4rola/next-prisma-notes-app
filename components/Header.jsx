9e0f3d631ef04788bbb2e6f1a2d343f5ec287515
Joe Froula <jmfroula@gmail.com>
2023-03-29 09:45:57 -0700

    updates header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button'; 
+import Link from 'next/link'; 
 
 function Header() {
   return (
-    <div className='d-flex flex-column align-items-center p-5'><h1>Tabbed Notes</h1><p>A simple CRUD application to keep track of things.</p></div>
+    <div className='d-flex flex-column align-items-center p-5'><h1>Tabbed Notes</h1><h5 className='text-center'>A simple CRUD application to keep track of things.</h5><p>Built with Next.js, Prisma, and CockroachDb</p>  
+    <Link href="https://github.com/j4rola/next-prisma-notes-app"><Button>See the Code</Button></Link></div>
   )
 }
 
